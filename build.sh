dd040e7f430bab0283d68ed7edeaf6c87a42d3cb
ikas <65895428+ikas-mc@users.noreply.github.com>
2023-12-17 17:24:36 +0800

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+BASE_PATH=$(pwd)
+export KBUILD_BUILD_HOST=github
+export KBUILD_BUILD_USER=github
+export ARCH=arm64
+echo ">${BASE_PATH}"
+
+# system
+echo ">install tools"
+sudo apt update -y 
+sudo apt install -y elfutils libarchive-tools
+
+#libufdt
+echo ">clone libufdt"
+git clone --branch android-13.0.0_r75 --depth 1 "https://android.googlesource.com/platform/system/libufdt.git" libufdt 
+
+#AnyKernel3
+echo ">clone AnyKernel3"
+git clone --depth 1 https://github.com/osm0sis/AnyKernel3  AnyKernel3
+
+# toolchain
+echo ">download toolchain"
+mkdir toolchain
+cd toolchain
+curl -LO "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman"
+chmod +x ./antman
+./antman -S
+./antman --patch=glibc
+cd $BASE_PATH
+
+#kernel
+echo ">clone kernel source"
+git clone --branch lineage-20 --depth 1 https://github.com/LineageOS/android_kernel_oneplus_sm8350 kernel
+
+#KernelSU
+echo ">clone KernelSU and patch the kernel"
+cd kernel
+curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s main
+cd $BASE_PATH
+
+#build
+echo ">build kernel"
+cd kernel
+export PATH="$BASE_PATH/toolchain/bin:${PATH}"
+MAKE_ARGS="CC=clang O=out ARCH=arm64 LLVM=1 LLVM_IAS=1"
+make $MAKE_ARGS "vendor/lahaina-qgki_defconfig"
+make $MAKE_ARGS -j"$(nproc --all)"
+cd $BASE_PATH
+cp kernel/out/arch/arm64/boot/Image AnyKernel3/
+
+#create dtb
+echo ">create dtb and dtbo.img"
+cat $(find kernel/out/arch/arm64/boot/dts/vendor/oplus/lemonadev/ -type f -name "*.dtb" | sort) > AnyKernel3/dtb
+python libufdt/utils/src/mkdtboimg.py create AnyKernel3/dtbo.img --page_size=4096 $(find kernel/out/arch/arm64/boot/dts/vendor/oplus/lemonadev/ -type f -name "*.dtbo" | sort)
+
+#clean AnyKernel3
+echo ">clean AnyKernel3"
+rm -rf AnyKernel3/.git* AnyKernel3/README.md
+echo "lineageOS oneplus sm8350 kernel with KernelSU" > AnyKernel3/README.md
+sed -i 's/do.devicecheck=1/do.devicecheck=0/g' AnyKernel3/anykernel.sh
+sed -i 's!block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;!block=auto;!g' AnyKernel3/anykernel.sh
+sed -i 's/is_slot_device=0;/is_slot_device=auto;/g' AnyKernel3/anykernel.sh
