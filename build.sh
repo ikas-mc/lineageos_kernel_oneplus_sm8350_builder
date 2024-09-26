ee4c4a949f5aa406e5cbfb082aed8b23ccd95e10
ikas <65895428+ikas-mc@users.noreply.github.com>
2024-09-26 12:47:34 +0800

    fix KernelSU and AnyKernel3

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ sudo apt install -y elfutils libarchive-tools
 
 #libufdt
 echo ">clone libufdt"
-git clone --branch android-13.0.0_r75 --depth 1 "https://android.googlesource.com/platform/system/libufdt.git" libufdt 
+git clone --branch android14-qpr2-release --depth 1 "https://android.googlesource.com/platform/system/libufdt.git" libufdt 
 
 #AnyKernel3
 echo ">clone AnyKernel3"
@@ -33,10 +33,10 @@ cd $BASE_PATH
 echo ">clone kernel source"
 git clone --branch lineage-21 --depth 1 https://github.com/LineageOS/android_kernel_oneplus_sm8350 kernel
 
-#KernelSU
+#KernelSU ( <=v0.9.5 only)
 echo ">clone KernelSU and patch the kernel"
 cd kernel
-curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s main
+curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s v0.9.5
 cd $BASE_PATH
 
 #build
@@ -54,10 +54,10 @@ echo ">create dtb and dtbo.img"
 cat $(find kernel/out/arch/arm64/boot/dts/vendor/oplus/lemonadev/ -type f -name "*.dtb" | sort) > AnyKernel3/dtb
 python libufdt/utils/src/mkdtboimg.py create AnyKernel3/dtbo.img --page_size=4096 $(find kernel/out/arch/arm64/boot/dts/vendor/oplus/lemonadev/ -type f -name "*.dtbo" | sort)
 
-#clean AnyKernel3
+#create AnyKernel3 zip
 echo ">clean AnyKernel3"
 rm -rf AnyKernel3/.git* AnyKernel3/README.md
-echo "lineageOS oneplus sm8350 kernel with KernelSU" > AnyKernel3/README.md
+echo "lineageOS-21 oneplus-sm8350 kernel with KernelSU" > AnyKernel3/README.md
 sed -i 's/do.devicecheck=1/do.devicecheck=0/g' AnyKernel3/anykernel.sh
-sed -i 's!block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;!block=auto;!g' AnyKernel3/anykernel.sh
-sed -i 's/is_slot_device=0;/is_slot_device=auto;/g' AnyKernel3/anykernel.sh
+sed -i 's!BLOCK=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;!BLOCK=auto;!g' AnyKernel3/anykernel.sh
+sed -i 's/IS_SLOT_DEVICE=0;/IS_SLOT_DEVICE=auto;/g' AnyKernel3/anykernel.sh
